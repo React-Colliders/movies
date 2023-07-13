9627fc98357336d17073c71eeb67d8482fbe316a
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-07-13 23:07:43 +0530

    movies

--- src/OldApp.js
+++ src/OldApp.js
@@ -15,22 +15,48 @@ const moviesData = [
     {name: "Jeena Sirf Mere Liye", rating: 4.5, year: "2002",img_link: "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTK7Os2YW_6OfJJGh9rvPUSbNYqUwQXZce6mMIrqMasLen8sg4BDbHwN-UMOAV6Q_lXdvqdhbY-NqCTcGA"}
 ]
 
-const App = () => {
+const OldApp = () => {
 
-//   let [selectedMovie, setSelectedMovie] = useState({})
+  let [selectedMovie, setSelectedMovie] = useState({})
 
-//   console.log(selectedMovie)
+  console.log(selectedMovie)
 
 
     return(
         <div className="movies">
-                <DisplayMovies />
+             <div className="all-movies">
+               {
+                     moviesData.map((movie, index) => (
+                            <div className="display-movies"
+                              onClick={()=>setSelectedMovie(movie)}
+                            >
+                                <h3>{movie.name}</h3>
+                            </div>
+                     ))
+               }
+               </div>
 
-                <SelectedMovies />
               
-                    
+                    {
+                        Object.keys(selectedMovie).length > 0 && (
+                            <div className="movie-details">
+                                <img src={selectedMovie.img_link} alt="movie-poster" />
+                                <h2>{selectedMovie.name}</h2>
+                                <p>{selectedMovie.rating}</p>
+                                <p>{selectedMovie.year}</p>
+                                
+
+                            </div>
+                        )
+                    }
+
+
+                    {/* {
+                        Object.keys(selectedMovie).length > 0 ? () : ("")
+                    } */}
+               
         </div>
     )
 }   
 
-export default App;
+// export default App;
