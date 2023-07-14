95828e1285815e9304a0e54c8cb1a279f2d562d8
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-07-14 21:27:19 +0530

    movis new approach

--- src/moviesData.js
+++ src/moviesData.js
@@ -1,6 +1,3 @@
-import React,{useState} from "react";
-import "./style.css"
-
 const moviesData = [
     {name: "The Shawshank Redemption", rating: 9.3, year: 1994, img_link: "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTK7Os2YW_6OfJJGh9rvPUSbNYqUwQXZce6mMIrqMasLen8sg4BDbHwN-UMOAV6Q_lXdvqdhbY-NqCTcGA"},
     {name: "The Godfather", rating: 9.2, year: 1972,
@@ -15,48 +12,4 @@ const moviesData = [
     {name: "Jeena Sirf Mere Liye", rating: 4.5, year: "2002",img_link: "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTK7Os2YW_6OfJJGh9rvPUSbNYqUwQXZce6mMIrqMasLen8sg4BDbHwN-UMOAV6Q_lXdvqdhbY-NqCTcGA"}
 ]
 
-const OldApp = () => {
-
-  let [selectedMovie, setSelectedMovie] = useState({})
-
-  console.log(selectedMovie)
-
-
-    return(
-        <div className="movies">
-                 <div className="all-movies">
-               {
-                     moviesData.map((movie, index) => (
-                            <div className="display-movies"
-                              onClick={()=>setSelectedMovie(movie)}
-                            >
-                                <h3>{movie.name}</h3>
-                            </div>
-                     ))
-               }
-                  </div>
-
-              
-                    {
-                        Object.keys(selectedMovie).length > 0 && (
-                            <div className="movie-details">
-                                <img src={selectedMovie.img_link} alt="movie-poster" />
-                                <h2>{selectedMovie.name}</h2>
-                                <p>{selectedMovie.rating}</p>
-                                <p>{selectedMovie.year}</p>
-                                
-
-                            </div>
-                        )
-                    }
-
-
-                    {/* {
-                        Object.keys(selectedMovie).length > 0 ? () : ("")
-                    } */}
-               
-        </div>
-    )
-}   
-
-// export default App;
+export default moviesData;
